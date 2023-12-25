7edfc798f7676d977b46e0c02c68b5bb7e9ff032
yonghoon1013 <jyh10105@gmail.com>
2023-12-25 21:28:11 +0900

    yh+jy 2128

--- app/components/detail/Detail.js
+++ app/components/detail/Detail.js
@@ -47,17 +47,18 @@ function Detail() {
 
 
     const likeLoad = async () => {
-        const sKey = sessionStorage.getItem("key");
+        const sKey = JSON.parse(sessionStorage.getItem("loginObj")).key;
         await axios.get(`/api/detail?sKey=${sKey}&objKey=${objKey}`)
             .then(res => {
                 setLikeCheck(res.data);
             })
     }
 
+   
 
     //클릭시
     const likeTest = async () => {
-        const sKey = sessionStorage.getItem("key");
+        const sKey = JSON.parse(sessionStorage.getItem("loginObj")).key;
         await axios.get(`/api/detail?sKey=${sKey}&objKey=${objKey}`)
             .then(res => {
                 if (res.data) {
