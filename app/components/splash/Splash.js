2df95600b11d12706a6e450ffa74f64bb7884c2d
Sovidi <lim132445@gmail.com>
2023-12-29 23:45:09 +0900

    home

--- app/components/splash/Splash.js
+++ app/components/splash/Splash.js
@@ -13,17 +13,19 @@ export default function Splash() {
   const autoFun = async () => {
     const loginBool = sessionStorage.getItem("loginBool"); //없으면 null값 나옴
     
-    if(loginBool == 'true'){//이전에 로그인 하고 로그아웃 안한 경우(바로 메인화면으로 가기)
-      navigation.push('/');
-      
-    }else{//로그인 한 적 없을 때 바로 intro(회원가입 화면)화면으로
-      elSplash.current.classList.add('noVisual');
-    }
+    setTimeout(() => {
+      if(loginBool == 'true'){//이전에 로그인 하고 로그아웃 안한 경우(바로 메인화면으로 가기)
+          navigation.push('/');
+      }else{//로그인 한 적 없을 때 바로 intro(회원가입 화면)화면으로
+        elSplash.current.classList.add('noVisual');
+      }
+    }, 2000);
 
   }//autoFun(); 함수정의
 
   useEffect(()=>{
     autoFun();
+    return clearTimeout(autoFun);
   })
 
   return (
---
+++ app/pages/home/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./styles.module.scss"
+import Home from '@/app/page'
+
+function page() {
+  return (
+    <Home/>
+  )
+}
+
+export default page
